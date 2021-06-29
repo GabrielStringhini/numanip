06d8676fc4a988684ed98f1e45b3bce36c6c36fe
gabriel <gfstringhini@gmail.com>
2021-06-28 22:35:06 -0300

    modificada funcao add para receber vario numeros

--- calc/math.go
+++ calc/math.go
@@ -1,9 +1,15 @@
 package calc
 
-func Add(i int, j int) int {
-	return i + j
+func Add(numbers ...int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
+	return sum
 }
 
-func Substract(i int, j int) int {
+func Subtract(i int, j int) int {
 	return i - j
 }
